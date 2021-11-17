8991da2080b1f4f64d03a1804ef2c8d17573e5b3
Sadi Nirloy <snirloy30@stuy.edu>
2021-11-17 17:03:00 -0500

    Working on Day3 part 1

---
+++ Day3.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Day3{
+	public static int numHouses (String str){
+		Map<String, Integer> houses = new HashMap <String, Integer> ();
+		int xcoor = 0;
+		int ycoor = 0;
+		houses.put (("" + xcoor + "," + ycoor), 1);
+		for (int i = 0; i < str.length(); i++){
+			// Use if statements
+		}
+
+		return houses.size();
+	}
+	public static void main (String[] args){
+		System.out.println("Salutations, planet!");
+	}
+}
