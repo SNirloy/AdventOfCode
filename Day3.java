f745af3da3e179d70494411432713df53cc7b6b3
Sadi Nirloy <snirloy30@stuy.edu>
2021-11-24 11:41:03 -0500

    Day3 Completed Parts 1 and 2

--- Day3.java
+++ Day3.java
@@ -6,26 +6,99 @@ public class Day3{
 		Map<String, Integer> houses = new HashMap <String, Integer> ();
 		int xcoor = 0;
 		int ycoor = 0;
-		houses.put (("" + xcoor + "," + ycoor), 1);
+		String key = ("" + xcoor + "," + ycoor);
+		houses.put (key, 1);
 		for (int i = 0; i < str.length(); i++){
 			// Use if statements
-			if (str.substring(i, i+1).equals("^"){
+			if (str.substring(i, i+1).equals("^")){
 				ycoor++;
 			}
-			if (str.substring(i, i+1).equals("v"){
+			else if (str.substring(i, i+1).equals("v")){
 				ycoor--;
 			}
-			if (str.substring(i, i+1).equals(">"){
+			else if (str.substring(i, i+1).equals(">")){
 				xcoor++;
 			}
-			if (str.substring(i, i+1).equals("<"){
+			else{
 				xcoor--;
+			}
+			key = ("" + xcoor + "," + ycoor);
+			if ( houses.containsKey (key)){
+				houses.replace(key, houses.get(key) + 1);
+			}
+			else{
+				houses.put(key, 1);
 			}
 		}
 
 		return houses.size();
 	}
-	public static void main (String[] args){
-		System.out.println("Salutations, planet!");
+
+	public static int roboHouses(String str){
+		Map<String, Integer> houses = new HashMap <String, Integer> ();
+		int santax = 0;
+		int santay = 0;
+		int robox = 0;
+		int roboy = 0;
+		String key = ("" + 0 + "," + 0);
+		houses.put (key, 2);
+		for (int i = 0; i < str.length(); i++){
+			// Use if statements
+			if (i % 2 == 0){
+				key = (santax + "," + santay);
+				if (str.substring(i, i+1).equals("^")){
+					santay++;
+				}
+				else if (str.substring(i, i+1).equals("v")){
+					santay--;
+				}
+				else if (str.substring(i, i+1).equals(">")){
+					santax++;
+				}
+				else{
+					santax--;
+				}
+				key = ("" + santax + "," + santay);
+			}
+			else{
+				key = (robox + "," + roboy);
+				if (str.substring(i, i+1).equals("^")){
+					roboy++;
+				}
+				else if (str.substring(i, i+1).equals("v")){
+					roboy--;
+				}
+				else if (str.substring(i, i+1).equals(">")){
+					robox++;
+				}
+				else{
+					robox--;
+				}
+				key = ("" + robox + "," + roboy );
+			}
+			if ( houses.containsKey (key)){
+				houses.replace(key, houses.get(key) + 1);
+			}
+			else{
+				houses.put(key, 1);
+			}
+		}		
+		return houses.size();
+	}
+
+	public static void main (String[] args) throws IOException{
+		// System.out.println("Salutations, planet!");
+		try{
+			String input = args[0];
+			File file = new File (input);
+			BufferedReader br = new BufferedReader (new FileReader(input));
+			String directions = br.readLine();
+			System.out.println(numHouses(directions));
+			System.out.println(roboHouses(directions));
+			br.close();
+		}
+		catch (FileNotFoundException e){
+			System.out.println("You done goofed, fool");
+		}
 	}
 }
