7436a15e63fd0a15a2dcc14fc55cb1ff8a49879f
Sadi Nirloy <snirloy30@stuy.edu>
2021-12-01 10:21:17 -0500

    Completed Day1 Part2

--- 2021/Day1.java
+++ 2021/Day1.java
@@ -23,9 +23,31 @@ public class Day1{
 			return counter;
 		}
 	}
+	public static int sumIncreases (String fileName) throws IOException {
+		BufferedReader reading = new BufferedReader(new FileReader (fileName));
+		int count = 0;
+		ArrayList<Integer> nums = new ArrayList<Integer>();
+		try{
+			while (true == true){
+				//System.out.println(current);
+				nums.add(Integer.parseInt(reading.readLine()));
+				if (nums.size() >= 4){
+					int sum1 = nums.get(nums.size() - 2) + nums.get(nums.size() - 3) + nums.get(nums.size() - 4);
+					int sum2 = nums.get(nums.size() - 1) + nums.get(nums.size() - 2) + nums.get(nums.size() - 3);
+					if (sum2 > sum1){
+						count++;
+					}
+				}
+			}
+		}catch (NumberFormatException ex){
+			//System.out.println(nums);
+			return count;
+		}
+	}
 	public static void main(String[] args) throws IOException{
 		//System.out.println("Salutations, globe");
 		String fileName = args[0];
 		System.out.println(increases(fileName));
+		System.out.println(sumIncreases(fileName));
 	}
 }
