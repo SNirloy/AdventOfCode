f31ac8f7e34a6abf8fffaead685e70eb645338c0
Sadi Nirloy <snirloy30@stuy.edu>
2022-11-26 15:33:42 -0500

    Completed 2015 Day 7 FINALLY

--- 2015/Day7.java
+++ 2015/Day7.java
@@ -2,43 +2,93 @@ import java.util.*;
 import java.io.*;
 
 public class Day7{
-	public static String toBinary(String num){
-		int deci = Integer.parseInt(num);
-		String inBin = "";
-		for (int i = (int)(Math.log(deci)/Math.log(2)); i > 0; i--){
-			if (Math.pow(2, i) <= deci){
-				inBin += "1";
-				deci -= Math.pow(2, i);
-			}
-			else{
-				inBin += "0";
+	public static HashMap<String, Integer> bytes = new HashMap<String, Integer>();
+	public static HashMap<String, String[]> tree = new HashMap<String, String[]>();
+
+	public static void main (String[] args) throws IOException{
+		//System.out.println("Hello");
+		BufferedReader reading = new BufferedReader( new FileReader( new File ("Day7.in")));
+		while( reading.ready()){
+			String[] line = reading.readLine().split(" ");
+			int len = line.length;
+			String target = line[len - 1];
+			String[] source;
+			switch (len){
+				case 3:
+					source = new String[1];
+					if(! isNum(line[0])){
+						source[0] = line[0];
+						tree.put(target, source);
+					} else{
+						int value = Integer.parseInt(line[0]);
+						bytes.put(target, value);
+					}
+					break;
+				case 4:
+					source = new String[2];
+					source[0] = line[1];
+					source[1] = line[0];
+					tree.put(target, source);
+					break;
+				case 5:
+					source = new String[3];
+					source[2] = line[1];
+					source[0] = line[0];
+					source[1] = line[2];
+					tree.put(target, source);
+					break;
 			}
 		}
-		inBin += ("" + deci);
-		return inBin;
+		reading.close();
+		System.out.println("a: " + pathing("a"));
 	}
 
-	public static int toDecimal(String bin){
-		int num = 0;
-		for (int i = 1; i <= bin.length(); i++){
-			int place = bin.length() - i;
-			String digit = bin.substring( place, place + 1);
-			if (digit.equals("1")){
-				num += Math.pow(2, (i - 1));
+	public static boolean isNum(String word){
+		boolean wordiness = true;
+		for (int i = 0; i < word.length(); i++){
+			wordiness = (wordiness && Character.isDigit(word.charAt(i)));
+		}
+		return wordiness;
+	}
+
+	public static int pathing( String wire){
+		if (isNum(wire)){ return Integer.parseInt(wire);}
+		if(! bytes.containsKey(wire)){
+			String[] sources = tree.get(wire);
+			int len = sources.length;
+			switch( len){
+				case 1:
+					bytes.put(wire, pathing(sources[0]));
+					break;
+				case 2:
+					int source = pathing(sources[0]);
+					bytes.put(wire, ~source);
+					break;
+				case 3:
+					int source0 = pathing(sources[0]);
+					int source1 = pathing(sources[1]);
+					switch (sources[2]){
+						case "AND": 
+							bytes.put(wire, source0 & source1);
+							break;
+						case "OR":
+							bytes.put(wire, source0 | source1);	
+							break;
+						case "RSHIFT":
+							bytes.put(wire, source0 >> source1);
+							break;
+						case "LSHIFT": 
+							bytes.put(wire, source0 << source1);
+							break;
+					}
+					break;
 			}
 		}
-		return num;
-	}
-	public static void main(String[] args){
-		//System.out.println("Nebuchadnezzer");
-
-	// Testing toBinary
-		System.out.println(toBinary("357"));
-		System.out.println(toBinary("8"));
-		for (int i = 0; i < 16; i++){
-			String test = ""+i;
-			System.out.print(toBinary(test) + "->");
-			System.out.println(toDecimal(toBinary(test)));
+		int val = bytes.get(wire);
+		if (val < 0){
+			val += Math.pow(2, 16);
+			bytes.put(wire, val);
 		}
-	}		
+		return bytes.get(wire);
+	}
 }
