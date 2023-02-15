0f2ba6e62dc8abd1a29aa9137734beb1724ec83c
Sadi Nirloy <snirloy30@stuy.edu>
2023-02-15 14:54:04 -0500

    Combinations

--- 2015/Day9.java
+++ 2015/Day9.java
@@ -1,4 +1,4 @@
-import java.io.*;
+ import java.io.*;
 import java.util.*;
 
 public class Day9{
@@ -34,37 +34,67 @@ public class Day9{
 			System.out.println(Arrays.toString(row));
 		}
 
-		int[] full = new int[len];
-		for (int i = 0; i < len; i++){
-			full[i] = i;
+		int[] choices = new int[distMat.length];
+		for (int i = 0; i < choices.length; i++){
+			choices[i] = i;
 		}
-		
-		System.out.println(holdingFish(distMat, full));
+	
+		System.out.println("Choices: " + Arrays.toString(choices));
+
+/*
+		ArrayList<int[]> singles = combinations(choices, new int[1], 0, new ArrayList<int[]>());
+
+		System.out.println("Combinations of 1");
+		for (int[] arr : singles){
+			System.out.println("\t" + Arrays.toString(arr));
+		}
+
+
+		ArrayList<int[]> pairs = combinations(choices, new int[2], 0, new ArrayList<int[]>());
+
+		System.out.println("Combinations of 2");
+		for (int[] arr : pairs){
+			System.out.println("\t" + Arrays.toString(arr));
+		}
+
+		ArrayList<int[]> triples = combinations(choices, new int[3], 0, new ArrayList<int[]>());
+
+		System.out.println("Combinations of 3");
+		for (int[] arr : triples){
+			System.out.println("\t" + Arrays.toString(arr));
+		}
+*/
+
 	}
 
-	public static int holdingFish( int[][] distMat, int[] places){
-		if(places.length > 2){
-			int minDist = Integer.MAX_VALUE;
-			for (int endpoint = 0; endpoint < places.length; endpoint ++){
-				int[] subset = new int[places.length - 1];
-			    // filling out the array
-				for (int i = 0; i < places.length; i++){
-					if (i < endpoint){
-						subset[i] = places[i];
-					}
-					if (i > endpoint){
-						subset[i - 1] = places[i];
-					}
+	public static ArrayList<int[]> combinations (int[] choices, int[] holding, int index, ArrayList<int[]> ultracombo){
+		for (int i = 0; i < choices.length; i++){
+
+	/*
+			System.out.println("i to index: " + i + ", " + index);
+			System.out.println("Choices: " + Arrays.toString(choices));
+			System.out.println("Holding: " + Arrays.toString(holding));
+			System.out.println("Combos: ");
+			for (int[] arr: ultracombo){System.out.println("\t" + Arrays.toString(arr));}
+			System.out.println();
+
+	*/
+			holding[index] = choices[i];
+			if (index == holding.length - 1){
+				int[] copy = new int[holding.length];
+				for (int j = 0; j < holding.length; j++){
+					copy[j] = holding[j];
 				}
-				int dist = holdingFish(distMat, subset);
-				dist += Math.min(distMat[subset[0]][places[endpoint]], distMat[subset[subset.length - 1]][places[endpoint]]);
-				minDist = Math.min (dist, minDist);
+				ultracombo.add(copy);
 			}
-			return minDist;
+			else{
+				int[] newChoices = new int[choices.length - i - 1 ];
+				for (int j = i + 1; j < choices.length; j++){
+					newChoices[j - i - 1] = choices[j];
+				}
+				combinations(newChoices, holding, index + 1, ultracombo);
+			}
 		}
-		else{
-			return distMat[places[0]][places[1]];
-		}
+		return ultracombo;
 	}
-	
 }
