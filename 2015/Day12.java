9ad8acf4c74846d519f3ae49ea7bdd818f8e443a
Sadi Nirloy <snirloy30@stuy.edu>
2023-03-23 19:38:07 -0400

    Day 12 part 1

---
+++ 2015/Day12.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Day12{
+	public static void main(String[] args) throws IOException{
+		BufferedReader reading = new BufferedReader(new FileReader( new File( "Day12.in")));
+		long sum = 0;
+		while (reading.ready()){
+			String line = reading.readLine();
+			String num = "";
+			boolean negative = false;
+			for (int i = 0; i < line.length(); i++){
+				char c = line.charAt(i);
+				if (c >= 48 && c <= 57){
+					num += c;
+				}
+				else{
+					if (num.length() > 0){
+						int trueNum = Integer.parseInt(num);
+						if (negative){trueNum *= -1;}
+						sum += trueNum;
+						System.out.println(trueNum);
+					}
+					num = "";
+					negative = false;
+				}	
+				if (c == 45){ negative = true;}			
+			}
+		}
+		reading.close();
+		System.out.println(sum);
+	}
+}
